87c8152d7651f7c13c0b5d04c7e9dcdcc5b9d383
Connor William Roy Egan <connorwilliamroy.egan@gmail.com>
2023-12-24 15:02:25 +1100

    Add Save System

---
+++ New Top Down Shooter/Assets/Scripts/ISerializer.cs
@@ -0,0 +1,10 @@
+namespace TimeStrike.SaveManagement.Serializers
+{
+    public interface ISerializer 
+    {
+        public string SerializeObject(object toSerialize);
+        public string SerializeAndEncrypt(object toSerialize, string encryptionKey);
+        public T DeserializeObject<T>(string json);
+        public T DecryptAndDeserialize<T>(string encryptedJson, string decryptionKey);
+    }
+}
